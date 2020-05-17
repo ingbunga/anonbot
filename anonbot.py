815fe01b60766a3e9af7b02f240e5f4aef852f60
ingbunga <kubeginner@naver.com>
2020-05-17 23:42:26 +0900

    Update anonbot.py

--- anonbot.py
+++ anonbot.py
@@ -5,14 +5,15 @@ music_channel_id = int(os.environ["music_channel_id"])
 class MyClient(discord.Client):
     async def on_message(self, message):
         if(str(message.channel)[:14] == 'Direct Message'and message.author.bot == False):
-            cmd = message.content.split(' ')[0]
             if message.content == '':
                 for i in message.attachments:
                     await client.get_channel(defalut_channel_id).send(file = await i.to_file())
-            elif cmd == '음악채널':
-                await client.get_channel(music_channel_id).send(message.content[5:])
             else:
-                await client.get_channel(defalut_channel_id).send(message.content)
+                cmd = message.content.split(' ')[0]
+                if cmd == '음악채널':
+                    await client.get_channel(music_channel_id).send(message.content[5:])
+                else:
+                    await client.get_channel(defalut_channel_id).send(message.content)
 client = MyClient()
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
