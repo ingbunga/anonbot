c32960cf0b37f5383f24c97803609df88b5bb970
ingbunga <kubeginner@naver.com>
2020-05-17 20:37:10 +0900

    Update anonbot.py

--- anonbot.py
+++ anonbot.py
@@ -5,7 +5,7 @@ music_channel_id = int(os.environ["music_channel_id"])
 class MyClient(discord.Client):
     async def on_message(self, message):
         if(str(message.channel)[:14] == 'Direct Message'and message.author.bot == False):
-            cmd = message.content.split(' ')[1]
+            cmd = message.content.split(' ')[0]
             if cmd == '음악채널':
                 await client.get_channel(music_channel_id).send(message.content[5:])
             else:
