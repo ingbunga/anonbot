f09ad407abb0707d1116f2f4ab67eb6fab1076d2
ingbunga <kubeginner@naver.com>
2020-05-17 20:16:42 +0900

    Update anonbot.py

--- anonbot.py
+++ anonbot.py
@@ -1,9 +1,10 @@
 import discord
 import os
+defalut_cannel_id = os.environ["default_cannel_id"]
 class MyClient(discord.Client):
     async def on_message(self, message):
         if(str(message.channel)[:14] == 'Direct Message'and message.author.bot == False):
-            await client.get_channel(os.environ["default_cannel_id"]).send(message.content)
+            await client.get_channel(defalut_cannel_id).send(message.content)
 client = MyClient()
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
